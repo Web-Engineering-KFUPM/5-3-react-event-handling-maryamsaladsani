bbcaab9bb4d567538befc67b7239e41e5c3e8a1a
maryamsaladsani <s202263480@kfupm.edu.sa>
2025-10-06 17:27:20 +0300

    all tasks are done

--- 5-3-react-event-handling/src/components/TaskList.jsx
+++ 5-3-react-event-handling/src/components/TaskList.jsx
@@ -4,20 +4,18 @@ import TaskItem from "./TaskItem";
 export default function TaskList({ tasks, onDelete }) {
   return (
       <ul className="list">
-        {if (tasks.length === 0) {
-          return <p className="placeholder">No tasks yet — add one ✨</p>;
-        }}
-
-        {tasks.map((task) => (
-            <TaskItem
-                key={task.id}
-                id={task.id}
-                text={task.text}
-                onDelete={onDelete}   // for Task 3 (Delete)
-            />
-        ))}
-
+          {tasks.length === 0 ? (
+              <li className="placeholder">No tasks yet — add one ✨</li>
+          ) : (
+              tasks.map((task) => (
+                  <TaskItem
+                      key={task.id}
+                      id={task.id}
+                      text={task.text}
+                      onDelete={onDelete}
+                  />
+              ))
+          )}
       </ul>
-
   );
 }
